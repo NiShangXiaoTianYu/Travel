53c74ea61c0bc02deaa9ef074fef9cc974636102
dzz <812831650@qq.com>
2019-04-14 23:01:42 +0800

    progect

---
+++ travel/src/store/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import state from './state'
+import mutations from './mutations'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state,
+  // 由于没有异步操作list页面可以直接调用mutations略过actions
+  // actions: {
+  //   changeCity (ctx, city) {
+  //     ctx.commit('changeCity', city)
+  //   }
+  // },
+  mutations
+})
